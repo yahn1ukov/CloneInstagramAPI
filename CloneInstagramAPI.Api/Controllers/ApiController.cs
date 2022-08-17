95f151bf47c7049e8087af214a59665874ca6752
Andrii Yahniukov <andrii.yahniukov@gmail.com>
2022-08-17 17:31:06 +0300

    feat: base controller & handle errors

---
+++ CloneInstagramAPI.Api/Controllers/ApiController.cs
@@ -0,0 +1,24 @@
+﻿using ErrorOr;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CloneInstagramAPI.Api.Controllers
+{
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        public IActionResult Problem(List<Error> errors)
+        {
+            var firstError = errors[0];
+
+            var statusCode = firstError.Type switch
+            {
+                ErrorType.Conflict => StatusCodes.Status409Conflict,
+                ErrorType.Validation => StatusCodes.Status400BadRequest,
+                ErrorType.NotFound => StatusCodes.Status404NotFound,
+                _ => StatusCodes.Status500InternalServerError
+            };
+
+            return Problem(statusCode: statusCode, detail: firstError.Description);
+        }
+    }
+}
---
+++ CloneInstagramAPI.Api/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿namespace CloneInstagramAPI.Api
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPresentention(this IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            return services;
+        }
+    }
+}
